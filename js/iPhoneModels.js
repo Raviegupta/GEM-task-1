a86dc32ebf9a50fff45df51c313e3e33690d6a2b
Raviegupta <raviegupta153@gmail.com>
2023-06-02 01:47:30 +0530

    task-1

---
+++ js/iPhoneModels.js
@@ -0,0 +1,44 @@
+// variables
+const iPhoneSearchInput = document.querySelector("#iPhone-search-input");
+const iphoneSearchBtn = document.querySelector("#iphone-search-btn");
+let iphoneListsBox = document.querySelector("#mobile-lists-box");
+let iphoneLists = document.querySelector(".mobiles-list");
+console.log(iphoneListsBox);
+console.log(iphoneLists);
+
+const iPhoneAvailable = [
+    "iphone 14 Pro Max",
+    "iPhone 14 Pro",
+    "iPhone 14 Plus",
+    "iPhone 14",
+    "iphone 13 Pro Max",
+    "iPhone 13 Pro",
+    "iPhone 13",
+    "iPhone 13 Mini",
+    "iphone 12 Pro Max",
+    "iPhone 12 Pro",
+    "iPhone 12",
+    "iPhone 12 Mini",
+    "iphone 11 Pro Max",
+  "iPhone 11 Pro",
+  "iPhone 11",
+  "iPhone XR",
+  "iphone XS Max",
+  "iPhone XS",
+  "iPhone X",
+  "iPhone 8 Plus",
+  "iPhone 8",
+  "iPhone 7 Plus",
+  "iPhone 7",
+  "iPhone 6S Plus",
+];
+
+const displayPhones = (iphoneLists) => {
+    iphoneListsBox.innerHTML = iphoneLists
+}
+
+for (let i=0; i<iPhoneAvailable.length; i++) {
+  iphoneLists += `<button>${iPhoneAvailable[i]}</button> `;
+}
+console.log(iphoneLists);
+displayPhones(iphoneLists);
